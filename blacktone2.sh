9c3ad3d85051acf3755752ec34cfee4c669a50cd
Kurone <zerrubabbel@gmail.com>
2026-06-21 17:10:36 -0400

    Implement environment checks in blacktone2.sh
    Added environment checks for user, desktop environment, OS, and systemd.

--- blacktone2.sh
+++ blacktone2.sh
@@ -1,1 +1,38 @@
-#blacktone2.sh
+# blacktone2.sh
+# ENVIRONMENT CHECKS
+
+if [[ $EUID -eq 0 ]]; then
+    echo "blacktone2.sh cannot be run as root"
+    exit 1
+fi
+
+if [[ "${XDG_CURRENT_DESKTOP:-}" != *GNOME* ]]; then
+    echo "Blacktone2 must be run from a GNOME session."
+    exit 1
+fi
+
+if [[ ! -f /etc/arch-release ]]; then
+    echo "This script requires Arch Linux."
+    exit 1
+fi
+
+if ! command -v systemctl >/dev/null; then
+    echo "systemd is required."
+    exit 1
+fi
+
+set -euo pipefail
+
+#SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+#THREADS=${THREADS:-$(nproc)} # prepped for Code Red setup script
+
+clear
+
+# RUN blacktone3.sh
+
+#git clone https://github.com/zerrubabbel/blacktone2
+
+#bash $HOME/PATH/TO/blacktone3.sh
+
+# REBOOT
+
